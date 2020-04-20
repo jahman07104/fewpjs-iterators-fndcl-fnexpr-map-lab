28678d6a507d128fcb267634dc7792cb3d16ebdb
jahman07104 <jahman@bellatlantic.net>
2020-04-20 14:12:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// const tutorials = [
+//   'what does the this keyword mean?',
+//   'What is the Contutorialuctor OO pattern?',
+//   'implementing Blockchain Web API',
+//   'The Test Driven Development Workflow',
+//   'What is NaN and how Can we Check for it',
+//   'What is the difference between stopPropagation and preventDefault?',
+//   'Immutable State and Pure Functions',
+//   'what is the difference between == and ===?',
+//   'what is the difference between event capturing and bubbling?',
+//   'what is JSONP?'
+// ];
+
+
+// const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
+//   word => word[0].toUpperCase() + word.slice(1)
+// ).join(' ')
+
+// )
+
+  
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Contutorialuctor OO pattern?',
@@ -11,9 +32,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-
-const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
-  word => word[0].toUpperCase() + word.slice(1)
-).join(' ')
-
-)
+const titleCased = (input) => {
+  return tutorials.map( line => {
+    let tokens = line.split(' ')
+    let capitalizedTokens =
+      tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) )
+    let response = capitalizedTokens.join(' ')
+    return response
+  })
+}
